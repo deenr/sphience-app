94e774cb783200c832361c64da2300ec2c40ba92
Dean Reymen <deenrayman@outlook.com>
2024-09-17 08:54:57 +0700

    chore: implement avatar component into sidenav

--- src/app/core/sidenav/sidenav.module.ts
+++ src/app/core/sidenav/sidenav.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
+import { AvatarComponent } from '@shared/components/avatar/avatar.component';
 import { SidenavItemComponent } from './sidenav-item/sidenav-item.component';
 import { SidenavProfileComponent } from './sidenav-profile/sidenav-profile.component';
 import { SidenavComponent } from './sidenav.component';
@@ -9,6 +10,6 @@ import { SidenavComponent } from './sidenav.component';
 @NgModule({
   exports: [SidenavComponent],
   declarations: [SidenavComponent, SidenavItemComponent, SidenavProfileComponent],
-  imports: [MatIconModule, MatDividerModule, RouterModule]
+  imports: [MatIconModule, MatDividerModule, RouterModule, AvatarComponent]
 })
 export class SidenavModule {}
