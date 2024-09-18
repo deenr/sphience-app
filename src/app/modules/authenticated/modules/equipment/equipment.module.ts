968e028d1d4f0567d60a685843ef485790b092be
Dean Reymen <deenrayman@outlook.com>
2024-09-18 13:28:00 +0700

    chore: equipment filters

--- src/app/modules/authenticated/modules/equipment/equipment.module.ts
+++ src/app/modules/authenticated/modules/equipment/equipment.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatMenuModule } from '@angular/material/menu';
 import { HeaderComponent } from '@shared/components/header/header.component';
 import { TableModule } from '@shared/components/table/table.module';
@@ -10,6 +13,6 @@ import { EquipmentComponent } from './pages/equipment/equipment.component';
 
 @NgModule({
   declarations: [EquipmentComponent],
-  imports: [EquipmentRoutingModule, HeaderComponent, MatButtonModule, MatIconModule, MatMenuModule, TableModule, TabsComponent]
+  imports: [EquipmentRoutingModule, HeaderComponent, MatButtonModule, MatIconModule, MatMenuModule, TableModule, TabsComponent, MatFormFieldModule, MatInputModule, MatButtonToggleModule]
 })
 export class EquipmentModule {}
--- src/app/modules/authenticated/modules/equipment/pages/equipment/equipment.component.ts
+++ src/app/modules/authenticated/modules/equipment/pages/equipment/equipment.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, signal, WritableSignal } from '@angular/core';
 import { TableColumnBuilder } from '@shared/components/table/builder/table-column-builder';
 import { TabsItem } from '@shared/components/tabs/tabs-item.interface';
 
 export type EquipmentTabKey = 'all' | 'available' | 'favourites';
+type EquipmentView = 'grid' | 'list';
 
 @Component({
   selector: 'app-equipment',
@@ -15,6 +16,8 @@ export class EquipmentComponent {
     { key: 'available', value: 'Available' },
     { key: 'favourites', value: 'Favourites' }
   ];
+
+  public $equipmentView: WritableSignal<EquipmentView> = signal('list');
 
   public data = [
     {
@@ -92,4 +95,8 @@ export class EquipmentComponent {
   ];
 
   public columns = [new TableColumnBuilder().setField('name').setHeaderName('Device name').build()];
+
+  public onViewChange(view: EquipmentView): void {
+    this.$equipmentView.set(view);
+  }
 }
