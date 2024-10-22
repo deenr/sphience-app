e417eb06b4b5aca71268e4224845c00430b0ee20
Dean Reymen <deenrayman@outlook.com>
2024-10-22 20:31:08 +0700

    chore: new device card

--- src/app/shared/components/device-card/device-card.component.ts
+++ src/app/shared/components/device-card/device-card.component.ts
@@ -1,36 +1,11 @@
 import { DatePipe } from '@angular/common';
-import { Component, input, Signal } from '@angular/core';
+import { Component, input, signal, WritableSignal } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { Equipment } from '@core/models/types/equipment.interface';
 import { AvatarGroupComponent, AvatarGroupSize } from '../avatar-group/avatar-group.component';
 import { BadgeComponent, BadgeSize, Color } from '../badge/badge.component';
 
-export interface BaseMicroscopeData {
-  name: string;
-  imageSrc: string;
-}
-
-export interface AvailableMicroscope extends BaseMicroscopeData {
-  availableUntil: Date;
-}
-
-export interface UnavailableMicroscope extends BaseMicroscopeData {
-  availableFrom: Date;
-}
-
-export interface PickupMicroscope extends BaseMicroscopeData {
-  reservationDate: Date;
-}
-
-export interface ReturnMicroscope extends BaseMicroscopeData {
-  reservationEndDate: Date;
-}
-
-export interface DocumentsMicroscope extends BaseMicroscopeData {
-  documentCount: number;
-  users: string[];
-}
-
-export type MicroscopeData = AvailableMicroscope | UnavailableMicroscope | PickupMicroscope | ReturnMicroscope | DocumentsMicroscope;
+export type DeviceCardView = 'availability' | 'documents';
 
 @Component({
   standalone: true,
@@ -40,66 +15,15 @@ export type MicroscopeData = AvailableMicroscope | UnavailableMicroscope | Picku
   imports: [MatIconModule, BadgeComponent, DatePipe, AvatarGroupComponent]
 })
 export class DeviceCardComponent {
-  public data = input.required<MicroscopeData>();
+  public data = input.required<Equipment>();
 
   public Color = Color;
   public BadgeSize = BadgeSize;
   public AvatarGroupSize = AvatarGroupSize;
 
-  public get availableData(): Signal<AvailableMicroscope> {
-    return this.data as Signal<AvailableMicroscope>;
-  }
+  public $currentCardView: WritableSignal<DeviceCardView> = signal('availability');
 
-  public get unavailableData(): Signal<UnavailableMicroscope> {
-    return this.data as Signal<UnavailableMicroscope>;
-  }
-
-  public get pickupData(): Signal<PickupMicroscope> {
-    return this.data as Signal<PickupMicroscope>;
-  }
-
-  public get returnData(): Signal<ReturnMicroscope> {
-    return this.data as Signal<ReturnMicroscope>;
-  }
-
-  public get documentsData(): Signal<DocumentsMicroscope> {
-    return this.data as Signal<DocumentsMicroscope>;
-  }
-
-  public get daysTillPickup(): number {
-    return this.getDiffBetweenToDays(new Date(), this.pickupData().reservationDate);
-  }
-
-  public get daysTillDropoff(): number {
-    return this.getDiffBetweenToDays(new Date(), this.returnData().reservationEndDate);
-  }
-
-  public isAvailableMicroscope(data: MicroscopeData): data is AvailableMicroscope {
-    return 'availableUntil' in data;
-  }
-
-  public isUnavailableMicroscope(data: MicroscopeData): data is UnavailableMicroscope {
-    return 'availableFrom' in data;
-  }
-
-  public isPickupMicroscope(data: MicroscopeData): data is PickupMicroscope {
-    return 'reservationDate' in data;
-  }
-
-  public isReturnMicroscope(data: MicroscopeData): data is ReturnMicroscope {
-    return 'reservationEndDate' in data;
-  }
-
-  public isDocumentsMicroscope(data: MicroscopeData): data is DocumentsMicroscope {
-    return 'documentCount' in data && 'users' in data;
-  }
-
-  private getDiffBetweenToDays(dateA: Date, dateB: Date): number {
-    const _MS_PER_DAY = 1000 * 60 * 60 * 24;
-
-    const utc1 = Date.UTC(dateA.getFullYear(), dateA.getMonth(), dateA.getDate());
-    const utc2 = Date.UTC(dateB.getFullYear(), dateB.getMonth(), dateB.getDate());
-
-    return Math.floor((utc2 - utc1) / _MS_PER_DAY);
+  public toggle(): void {
+    this.$currentCardView.update((current) => (current === 'availability' ? 'documents' : 'availability'));
   }
 }
